51edd6dd706567289e0e2efacdcabfb17811bd68
Keerthana Srinivasan <keerthana20011122@gmail.com>
2025-12-24 15:02:14 -0600

    hyper parameter training

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,11 @@
 import os
 import sys
 import dill
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
-from sklearn.metrics import r2_score
+
 
 def save_object(file_path: str, obj) -> None:
     """
@@ -19,26 +21,36 @@ def save_object(file_path: str, obj) -> None:
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
 
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    """
+    For each model:
+    - run GridSearchCV using param grid
+    - fit best model
+    - return test R2 score in a dict
+    """
     try:
-        report = {} 
+        report = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
+        for model_name, model in models.items():
+            para = param.get(model_name, {})
 
-            model.fit(X_train, y_train)
+            # If there are no params, just train directly
+            if len(para) == 0:
+                model.fit(X_train, y_train)
+            else:
+                gs = GridSearchCV(model, para, cv=3, n_jobs=-1, scoring="r2")
+                gs.fit(X_train, y_train)
 
-            y_train_pred = model.predict(X_train)
+                model.set_params(**gs.best_params_)
+                model.fit(X_train, y_train)
 
             y_test_pred = model.predict(X_test)
-
-            train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            report[model_name] = test_model_score
 
         return report
-    
+
     except Exception as e:
         raise CustomException(e, sys)
